5b69ca50f2f9e4da16efb78bf6343f5f48bad453
mattperkins <github@mattperkins.com>
2018-08-29 22:23:30 +0100

    add axios

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,36 @@
 import * as React from 'react'
+import axios from 'axios'
+import {Link} from 'react-router-dom'
 
-export default () => (
-<div>
- <h1>Home</h1>
- <p>...</p>
-</div>
-)
+class Home extends React.Component{
+ state = {
+  posts: []
+ }
+ componentDidMount(){
+  axios.get('https://jsonplaceholder.typicode.com/posts')
+  .then(res => {
+   this.setState({
+    posts: res.data.slice(0,10)
+   })
+  })
+ }
+ render(){
+  const {posts} = this.state
+  const postList = posts.length ? (
+   posts.map(post => {
+    return(
+     <div key={post.id}>
+      <Link to={'/' + post.id}>
+       <h1>{post.title}</h1>
+       <p>{post.body}</p>
+      </Link>
+     </div>
+    )
+   })
+  ) : <p>Loading posts...</p>
+  return(
+    <div>{postList}</div>
+  )
+ }
+}
+export default Home
--- src/components/Post.js
+++ src/components/Post.js
@@ -1,18 +1,25 @@
 import * as React from 'react'
+import axios from 'axios'
 
 class Post extends React.Component{
  state = {
-  id: null
+  post: null
  }
  componentDidMount(){
   // console.log(this.props)
   let id = this.props.match.params.post_id
-  this.setState({id: id})
+  axios.get('https://jsonplaceholder.typicode.com/posts/' + id)
+   .then(res => {
+     this.setState({
+      post: res.data
+     })
+     console.log(res)
+   })
  }
  render(){
   return(
    <div>
-     <h4>{this.state.id}</h4>
+    
    </div>
   )
  }
