dd3f23cee339c7883273c2a391bcb00dab9c840e
mattperkins <github@mattperkins.com>
2018-08-31 17:32:10 +0100

    add image var + env BROWSER=none

--- src/components/About.js
+++ src/components/About.js
@@ -1,7 +1,9 @@
 import * as React from 'react'
+const img = 'http://dormshed.com/static/img/shed.svg'
 
 export default () => (
 <div>
+ <img src={img} alt="DORMshed"/>
  <h1>About</h1>
  <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sint delectus repellendus esse mollitia dicta aliquid molestias temporibus reiciendis et! Ipsa doloribus facilis voluptatum, cum blanditiis omnis nobis eius repudiandae repellat.</p>
 </div>
