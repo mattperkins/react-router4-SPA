1975cebff3a941084d7f86f4da9b438ba0365d59
mattperkins <github@mattperkins.com>
2018-09-12 04:03:47 +0100

    cl

--- src/App.js
+++ src/App.js
@@ -3,10 +3,10 @@ import Navbar from './components/Navbar'
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import About from './components/About'
 import Contact from './components/Contact'
-import Home from './components/Home'
-import Post from './components/Post'
-// import LocalPosts from './components/LocalPosts'
-// import LocalPost from './components/LocalPost'
+// import Home from './components/Home'
+// import Post from './components/Post'
+import LocalPosts from './components/LocalPosts'
+import LocalPost from './components/LocalPost'
 
 class App extends React.Component{
  render(){
@@ -15,11 +15,11 @@ class App extends React.Component{
      <div className="wrapper">
       <Navbar /> 
       <Switch>
-        <Route exact path='/' component={Home} />
-        <Route path='/:post_id' component={Post} /> 
+        {/* <Route exact path='/' component={Home} />
+        <Route path='/:post_id' component={Post} /> */}
 
-        {/* <Route exact path='/' component={LocalPosts} />
-        <Route path='/:localPost_id' component={LocalPost} /> */}
+        <Route exact path='/' component={LocalPosts} />
+        <Route path='/:localPost_id' component={LocalPost} /> 
 
         <Route path='/about' component={About} />
         <Route path='/contact' component={Contact} />
